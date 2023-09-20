27ec292b7a2ddfed16dec3a7213c32810333b4e0
alshkre9 <polyex9@gmail.com>
2023-09-20 17:09:12 +0200

    add views

---
+++ resources/views/product/store.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="/products/store/" method="post" enctype="multipart/form-data">
+        @csrf
+        <input type="text" name="name" placeholder="name">
+        <input type="file" name="image", placeholder="image">
+        <input type="text" name="quantity" placeholder="quantity">
+        <input type="text" name="description" placeholder="description">
+        <button type="submit">submit</button>
+    </form>
+</body>
+</html>
