f064775553e9d5ca3935c8819d8d54f48a0d22d7
alshkre9 <polyex9@gmail.com>
2023-10-07 17:51:25 +0200

    remove dashboard route and remove timestamps in images table

--- database/migrations/2023_10_04_172517_create_images_table.php
+++ database/migrations/2023_10_04_172517_create_images_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string("name")->nullable();
             $table->foreignId("product_id")->nullable();
         });
